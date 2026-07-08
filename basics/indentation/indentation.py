7fdb063678d42c89aa081d621b66ddbf671f164a
Moustafa Ahmad <moustafaahmed51015@gmail.com>
2026-07-08 23:29:54 +0300

    indentation implementation code

---
+++ basics/indentation/indentation.py
@@ -0,0 +1,27 @@
+#1. Missing indentation after a colon (:)
+
+# Incorrect:
+
+if x > 10:
+print("Greater")
+
+# Correct:
+
+if x > 10:
+    print("Greater")
+
+#==========================================
+
+#2. Inconsistent indentation
+
+# Incorrect:
+
+if x > 10:
+    print("Hello")
+      print("World")
+
+#  Correct:
+
+if x > 10:
+    print("Hello")
+    print("World")
